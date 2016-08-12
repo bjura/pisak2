c36160a036e57364518bf8f8ab9c5e730bc5029a
Bartosz Jura <bjura@braintech.pl>
2016-08-12 13:17:28 +0200

    Draft func for fonts installation

--- pisak/application.py
+++ pisak/application.py
@@ -1,11 +1,14 @@
 import sys
+import os
+import shutil
 
-from PyQt5.QtCore import QSettings, QObject, pyqtProperty
+from PyQt5.QtCore import QSettings, QObject, pyqtProperty, QStandardPaths
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
 from .resources import Resources
 from .settings import Settings
+from .res import getRes
 
 
 class _ApplicationContext(QObject):
@@ -31,6 +34,7 @@ class Application:
     name = 'pisak'
 
     def __init__(self, gui):
+        self._setupEnv()
         self._app = QApplication(sys.argv)
         self._app.setOrganizationName(self.organization)
         self._app.setApplicationName(self.name)
@@ -49,3 +53,29 @@ class Application:
     def _registerTypes(self):
         from .prediction import predictor
         predictor.registerTypes()
+
+    def _setupEnv(self):
+        self._installFonts()
+
+    def _installFonts(self):
+        fontDirs = getRes('fonts')
+        systemFontsDir = QStandardPaths.writableLocation(QStandardPaths.FontsLocation)
+        if not os.path.exists(systemFontsDir):
+            platform = sys.platform
+            if not (platform.startswith('linux') or platform.startswith('win')):
+                return
+            try:
+                os.mkdir(systemFontsDir)
+            except OSError as err:
+                print(err)
+                return
+        for fontName in os.listdir(fontDirs):
+            font = os.path.join(fontDirs, fontName)
+            for fontFileName in os.listdir(font):
+                fontFile = os.path.join(font, fontFileName)
+                targetFile = os.path.join(systemFontsDir, fontFileName)
+                if not os.path.exists(targetFile):
+                    try:
+                        shutil.copy2(fontFile, targetFile)
+                    except OSError as err:
+                        print(err)
